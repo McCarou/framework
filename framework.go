c4e45f50d9ab4cd2624ac03b5f08bda8027227c2
radianteam <radianlmsteam@gmail.com>
2022-10-01 22:12:42 +0300

    Framework pre and post tasks implementation began

--- framework.go
+++ framework.go
@@ -15,12 +15,16 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/radianteam/framework/worker"
+	"github.com/radianteam/framework/worker/task/job"
 )
 
 type WorkersMap map[string]worker.WorkerInterface
 
 // Main framework structure that holds worker list and global logger.
 type RadianFramework struct {
+	preTasks  []*job.TaskJob
+	postTasks []*job.TaskJob
+
 	workers WorkersMap
 	logger  *logrus.Entry
 }
@@ -41,7 +45,7 @@ func NewRadianFramework() *RadianFramework {
 
 // AddWorker registers a worker by name from Worker.GetName().
 // If the worker with the same name is already registred the
-// first one will be overwritten by the new one
+// first one will be overwritten by the new one.
 func (r *RadianFramework) AddWorker(w worker.WorkerInterface) {
 	if r.workers == nil {
 		r.workers = make(WorkersMap)
@@ -50,24 +54,55 @@ func (r *RadianFramework) AddWorker(w worker.WorkerInterface) {
 	r.workers[w.GetName()] = w
 }
 
-// Main framework loop. The loop setups adapters, captures the
-// thread and wait for SIGINT or SIGTERM signals.
-func (r *RadianFramework) Run(services []string) {
+// AddPreTask registers a list of functions that will be executed
+// before starting the main loop. Use it to get tokens, make
+// migrations, etc.
+func (r *RadianFramework) AddPreTask(t *job.TaskJob) {
+	r.preTasks = append(r.preTasks, t)
+}
+
+// AddPostTask registers a list of functions that will be executed
+// after finishing the main loop. Use it to invalidate tokens, make
+// termination signals, etc.
+func (r *RadianFramework) AddPostTask(t *job.TaskJob) {
+	r.postTasks = append(r.postTasks, t)
+}
+
+// Main framework loop. The loop setups adapters, runs pretasks,
+// captures the, thread and wait for SIGINT or SIGTERM signals.
+// After termination runs posttasks and releases the thread.
+func (r *RadianFramework) Run(_preTasks []string, _workers []string, _postTasks []string) {
 	logrus.SetFormatter(&logrus.JSONFormatter{})
 
 	r.logger.Info("running")
 
+	// check pretask names
+	for _, jobName := range _preTasks {
+		if _, ok := r.workers[jobName]; !ok {
+			r.logger.Fatalf("pretask with name %s is not found", jobName)
+		}
+	}
+
 	// check service names
-	for _, serviceName := range services {
+	for _, serviceName := range _workers {
 		if _, ok := r.workers[serviceName]; !ok {
 			r.logger.Fatalf("worker with name %s is not found", serviceName)
 		}
 	}
 
-	// run services
+	// check posttask names
+	for _, jobName := range _postTasks {
+		if _, ok := r.workers[jobName]; !ok {
+			r.logger.Fatalf("posttask with name %s is not found", jobName)
+		}
+	}
+
+	// run pretasks
+
+	// run workers
 	wg := sync.WaitGroup{}
 
-	for _, serviceName := range services {
+	for _, serviceName := range _workers {
 		wg.Add(1)
 
 		go func(name string) {
@@ -105,7 +140,7 @@ func (r *RadianFramework) Run(services []string) {
 
 	r.logger.Info("stopping workers")
 
-	for _, serviceName := range services {
+	for _, serviceName := range _workers {
 		r.workers[serviceName].Stop()
 	}
 
