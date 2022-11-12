829bb928a597f7bdba9c50adb112c639a36e1fef
McCarou <alex-mak96@mail.ru>
2022-11-12 21:06:53 +0300

    Reverts worker to previous implementation

--- worker/event/sqs/worker.go
+++ worker/event/sqs/worker.go
@@ -6,7 +6,10 @@ import (
 	sqs_adapter "github.com/radianteam/framework/adapter/event/sqs"
 	"github.com/radianteam/framework/worker"
 	"sync"
+	"time"
 )
+
+const RetryConsumeTimeoutMs = 10000
 
 type SqsWorkerRegFunc func(d *sqs.Message, wc *worker.WorkerAdapters) error
 
@@ -15,8 +18,9 @@ type SqsEventsWorker struct {
 
 	config *sqs_adapter.SqsConfig
 
-	mutex    sync.Mutex
-	waitChan chan struct{}
+	mutex       sync.Mutex
+	waitChan    chan struct{}
+	stopPolling bool
 
 	handlers map[string]SqsWorkerRegFunc
 }
@@ -48,8 +52,6 @@ func (w *SqsEventsWorker) Run() {
 		return
 	}
 
-	w.waitChan = make(chan struct{})
-
 	for queueName, handler := range w.handlers {
 		wg.Add(1)
 
@@ -58,51 +60,54 @@ func (w *SqsEventsWorker) Run() {
 
 			w.Logger.Infof("Consuming queue '%s'", qName)
 			for {
-				select {
-				case <-w.waitChan:
-					w.Logger.Infof("Consuming queue '%s' stopped", qName)
+				if w.stopPolling {
+					break
+				}
 
-					return
-				default:
-					msgs, err := adapter.Consume(qName)
+				msgs, err := adapter.Consume(qName)
+				if err != nil {
+					w.Logger.Errorf("Consuming queue '%s' started with error: %v", qName, err)
+
+					time.Sleep(RetryConsumeTimeoutMs * time.Millisecond)
+					continue
+				}
+
+				for _, message := range msgs {
+					w.Logger.Infof("Received a message from '%s'", qName)
+					w.Logger.Debugf("Received message body: '%s'", aws.StringValue(message.Body))
+
+					// Single thread processing. Adapters can be none thread safe!
+					w.mutex.Lock()
+					err = handler(message, w.Adapters)
+					w.mutex.Unlock()
+
 					if err != nil {
-						w.Logger.Errorf("Consuming queue '%s' started with error: %v", qName, err)
+						w.Logger.Errorf("Queue '%s' failed to proceed the message '%s ", qName, message)
 
 						return
 					}
 
-					for _, message := range msgs {
-						w.Logger.Infof("Received a message from '%s'", qName)
-						w.Logger.Debugf("Received message body: '%s'", aws.StringValue(message.Body))
+					err = adapter.DeleteMessage(qName, *message.ReceiptHandle)
+					if err != nil {
+						w.Logger.Errorf("Failed to delete the message '%s' from queue '%s'", message, qName)
 
-						// Single thread processing. Adapters can be none thread safe!
-						w.mutex.Lock()
-						err = handler(message, w.Adapters)
-						w.mutex.Unlock()
-
-						if err != nil {
-							w.Logger.Errorf("Queue '%s' failed to proceed the message '%s ", qName, message)
-
-							return
-						}
-
-						err = adapter.DeleteMessage(qName, *message.ReceiptHandle)
-						if err != nil {
-							w.Logger.Errorf("Failed to delete the message '%s' from queue '%s'", message, qName)
-
-							return
-						}
+						return
 					}
 				}
+
+				w.Logger.Infof("Consuming queue '%s' stopped", qName)
 			}
 		}(queueName, handler)
 	}
 
+	w.waitChan = make(chan struct{})
+	<-w.waitChan
 	wg.Wait()
 }
 
 func (w *SqsEventsWorker) Stop() {
 	w.Logger.Info("stop signal received! Graceful shutting down")
 
+	w.stopPolling = true
 	close(w.waitChan)
 }
