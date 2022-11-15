782ae0c1924eb0196f010d3483ad7312267cc6e5
radianteam <radianlmsteam@gmail.com>
2022-11-16 01:17:26 +0300

    Base adapter arguments support

--- worker/worker.go
+++ worker/worker.go
@@ -1,6 +1,8 @@
 package worker
 
 import (
+	"fmt"
+
 	"github.com/radianteam/framework/adapter"
 	"github.com/sirupsen/logrus"
 )
@@ -19,6 +21,8 @@ type WorkerInterface interface {
 	Stop()
 	SetMonitoring(enabled bool)
 	IsMonitoringEnable() bool
+	SetArgument(string, string)
+	GetArgument(string) (string, error)
 }
 
 // Worker structure contains an adapter list and implements
@@ -30,6 +34,7 @@ type BaseWorker struct {
 	monitoringEnabled bool
 	Logger            *logrus.Entry
 	Adapters          *WorkerAdapters
+	arguments         map[string]string
 }
 
 // Function allocates BaseWorker structure with JSON logger
@@ -47,6 +52,7 @@ func NewBaseWorker(name string) *BaseWorker {
 		Logger:            logger.WithField("worker", name),
 		Adapters:          NewWorkerAdapters(),
 		monitoringEnabled: false,
+		arguments:         make(map[string]string),
 	}
 }
 
@@ -92,4 +98,16 @@ func (w *BaseWorker) SetMonitoring(enabled bool) {
 // Function returns monitoring status.
 func (w *BaseWorker) IsMonitoringEnable() bool {
 	return w.monitoringEnabled
+}
+
+func (w *BaseWorker) SetArgument(name string, value string) {
+	w.arguments[name] = value
+}
+
+func (w *BaseWorker) GetArgument(name string) (string, error) {
+	if val, ok := w.arguments[name]; ok {
+		return val, nil
+	}
+
+	return "", fmt.Errorf("argument %s is not found", name)
 }
