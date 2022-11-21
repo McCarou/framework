dc08561581054b91dfaee187fe9ae8e992eaa1ee
radianteam <radianlmsteam@gmail.com>
2022-11-21 22:12:27 +0300

    OIDC adapter style

--- adapter/auth/oidc/adapter.go
+++ adapter/auth/oidc/adapter.go
@@ -33,17 +33,20 @@ type OidcAdapter struct {
 
 	config           *OidcConfig
 	provider         *oidc.Provider
-	verifier         *oidc.IDTokenVerifier
 	staticPublicKeys *oidc.StaticKeySet
 }
 
 func NewOidcAdapter(name string, config *OidcConfig) *OidcAdapter {
-	return &OidcAdapter{BaseAdapter: adapter.NewBaseAdapter(name), config: config, staticPublicKeys: &oidc.StaticKeySet{PublicKeys: []crypto.PublicKey{}}}
+	return &OidcAdapter{
+		BaseAdapter:      adapter.NewBaseAdapter(name),
+		config:           config,
+		staticPublicKeys: &oidc.StaticKeySet{PublicKeys: []crypto.PublicKey{}}}
 }
 
 func (a *OidcAdapter) Setup() (err error) {
 	if !a.config.OfflineMode {
 		a.provider, err = oidc.NewProvider(context.TODO(), a.config.ProviderUrl)
+
 		if err != nil {
 			logrus.WithField("adapter", a.GetName()).Errorf("cannot craete new provider - %s", err)
 		}
@@ -52,12 +55,14 @@ func (a *OidcAdapter) Setup() (err error) {
 	if len(a.config.PublicKeys) > 0 {
 		for _, pubPEM := range a.config.PublicKeys {
 			block, _ := pem.Decode([]byte(pubPEM))
+
 			if block == nil {
 				logrus.WithField("adapter", a.GetName()).Errorf("failed to parse PEM block containing the public key")
 				continue
 			}
 
 			pub, err := x509.ParseCertificate(block.Bytes)
+
 			if err != nil {
 				logrus.WithField("adapter", a.GetName()).Errorf("failed to parse DER encoded public key: %s", err)
 				continue
@@ -81,6 +86,7 @@ func (a *OidcAdapter) GetVerifier() *oidc.IDTokenVerifier {
 	} else if a.provider != nil {
 		return a.provider.Verifier(&oidc.Config{ClientID: a.config.ClientId, SkipIssuerCheck: a.config.SkipIssuerCheck})
 	}
+
 	logrus.WithField("adapter", a.GetName()).Errorf("cannot get verifier - provider empty")
 	return nil
 }
@@ -88,6 +94,7 @@ func (a *OidcAdapter) GetVerifier() *oidc.IDTokenVerifier {
 // Introspect - remote keycloak function is being called. Before the call, add client_id and client_secret in settings app.
 func (a *OidcAdapter) Introspect(token string) (err error) {
 	tokenURL := ""
+
 	if a.config.OfflineMode {
 		tokenURL = a.config.ProviderUrl + "/protocol/openid-connect/token"
 	} else if a.provider != nil {
@@ -104,6 +111,7 @@ func (a *OidcAdapter) Introspect(token string) (err error) {
 
 	client := &http.Client{}
 	res, err := client.PostForm(tokenURL+"/introspect", requestData)
+
 	if err != nil {
 		logrus.WithField("adapter", a.GetName()).Errorf("cannot introspect token - %s", err)
 		return
@@ -118,6 +126,7 @@ func (a *OidcAdapter) Introspect(token string) (err error) {
 	}
 
 	buf, err := io.ReadAll(res.Body)
+
 	if err != nil {
 		logrus.WithField("adapter", a.GetName()).Errorf("cannot read response body - %s", err)
 		return
@@ -125,6 +134,7 @@ func (a *OidcAdapter) Introspect(token string) (err error) {
 
 	tokenInfo := make(map[string]any)
 	err = json.Unmarshal(buf, &tokenInfo)
+
 	if err != nil {
 		logrus.WithField("adapter", a.GetName()).Errorf("cannot unmarshal raw token data - %s", err)
 		return
@@ -136,6 +146,7 @@ func (a *OidcAdapter) Introspect(token string) (err error) {
 		return errors.New("token expired")
 	} else {
 		expr := time.Unix(int64(tokenInfo["exp"].(float64)), 0)
+
 		if time.Now().UTC().After(expr.UTC()) {
 			return errors.New("token expired")
 		}
@@ -146,13 +157,19 @@ func (a *OidcAdapter) Introspect(token string) (err error) {
 
 func (a *OidcAdapter) VerifyToken(token string) (err error) {
 	tokenInfo, err := a.TokenInfo(token)
+
 	if err != nil {
 		return
 	}
 
 	customStruct := &struct{ Active *bool }{}
 
-	_ = tokenInfo.Claims(customStruct)
+	err = tokenInfo.Claims(customStruct)
+
+	if err != nil {
+		return errors.New("token claim error")
+	}
+
 	if customStruct.Active != nil && !*customStruct.Active || time.Now().UTC().After(tokenInfo.Expiry.UTC()) {
 		return errors.New("token expired")
 	}
@@ -162,15 +179,19 @@ func (a *OidcAdapter) VerifyToken(token string) (err error) {
 
 func (a *OidcAdapter) TokenInfo(token string) (tokenInfo *oidc.IDToken, err error) {
 	verifier := a.GetVerifier()
+
 	if verifier == nil {
 		err = errors.New("verifier is empty")
 		logrus.WithField("adapter", a.GetName()).Errorf("cannot get token info - %s", err)
+
 		return
 	}
 
 	tokenInfo, err = verifier.Verify(context.TODO(), token)
+
 	if err != nil {
 		logrus.WithField("adapter", a.GetName()).Errorf("cannot get token info - %s", err)
 	}
+
 	return
 }
